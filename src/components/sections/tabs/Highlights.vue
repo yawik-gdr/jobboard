afb1a585b3cbc0e861065e690a06225c03b0c1a3
Carsten Bleek <bleek@cross-solution.de>
2022-01-29 18:14:00 +0100

    feat: one day ago, 2 days ago, one week ago, 2 weeks ago ...

--- src/components/sections/tabs/Highlights.vue
+++ src/components/sections/tabs/Highlights.vue
@@ -16,10 +16,10 @@
                   <q-img fit="contain" :src="jobHost + val.attributes.logo.formats.thumbnail.url" height="50px"
                          width="100px"
                   />
-                  <q-item-label align="left"
+                  <q-item-label align="center"
                                 caption
                   >
-                    {{ $t('days', [diff(today,val.attributes.createdAt)]) }}
+                    {{ since(val.attributes.createdAt) }}
                   </q-item-label>
                 </q-item-section>
                 <q-item-section>
@@ -44,8 +44,8 @@
               >
                 <q-item-section avatar>
                   <q-img fit="contain" :src="val.organizationLogo" width="100px" height="50px" />
-                  <q-item-label align="left" caption>
-                    {{ $t('days',[diff(today,new Date(val.dateStart))]) }}
+                  <q-item-label align="center" caption>
+                    {{ since(val.dateStart) }}
                   </q-item-label>
                 </q-item-section>
 
@@ -108,10 +108,6 @@ export default {
         {
           return process.env.YAWIK_JOB_URL;
         },
-        diff()
-        {
-          return getDateDiff;
-        }
       },
   mounted()
   {
@@ -250,6 +246,52 @@ export default {
         rowsPerPage: pagination.pageSize
       };
     },
+    since(time)
+    {
+      const s = getDateDiff(this.today, time, 'seconds');
+      const day = 86400;
+      const week = 7 * day;
+      const month = 4 * week;
+      let text = '';
+
+      if (s < day)
+      {
+        text = this.$t('today');
+      }
+      else if (s >= day && s < 2 * day)
+      {
+        text = this.$t('yesterday');
+      }
+      else if (s >= 2 * day && s < 7 * day)
+      {
+        text = this.$t('n-days', [Math.round(s / day)]);
+      }
+      else if (s >= 7 * day && s < 14 * day)
+      {
+        text = this.$t('1-week');
+      }
+      else if (s >= 14 * day && s < 30 * day)
+      {
+        text = this.$t('n-weeks', [Math.round(s / week)]);
+      }
+      else if (s >= 30 * day && s < 60 * day)
+      {
+        text = this.$t('1-month');
+      }
+      else if (s >= 60 * day && s < 365 * day)
+      {
+        text = this.$t('n-months', [Math.round(s / month)]);
+      }
+      else if (s >= 365 * day && s < 750 * day)
+      {
+        text = this.$t('1-year');
+      }
+      else if (s >= 750 * day)
+      {
+        text = this.$t('n-years', [Math.round(s / (12 * month))]);
+      }
+      return text;
+    }
   }
 };
 </script>
@@ -280,15 +322,41 @@ export default {
   "en": {
     "search-placeholder": "Job title, Company or Location",
     "first-ad": "Place your first ad! Free of charge.",
-    "days": "{0} days ago",
+    "today": "today",
+    "yesterday": "yesterday",
+    "n-days": "{0} days ago",
+    "1-week": "one week ago",
+    "n-weeks": "{0} weeks ago",
+    "1-month": "one month ago",
+    "n-months": "{0} months ago",
+    "1-year": "one year ago",
+    "n-years": "{0} years ago"
   },
   "de": {
     "search-placeholder": "Anzeigentitel, Firma oder Ort",
     "first-ad": "Schalten sie ihre erste Anzeige! Kostenlos.",
-    "days": "vor {0} Tagen"
+    "today": "Heute",
+    "yesterday": "Gestern",
+    "n-days": "vor {0} Tagen",
+    "1-week": "vor einer Woche",
+    "n-weeks": "vor {0} Wochen",
+    "1-month": "vor einem Monat",
+    "n-months": "vor {0} Monaten",
+    "1-year": "vor einem Jahr",
+    "n-years": "vor {0} Jahren"
   },
   "fr": {
-    "days": "il y a {0} jours"
-  }
+    "search-placeholder": "Anzeigentitel, Firma oder Ort",
+    "first-ad": "Schalten sie ihre erste Anzeige! Kostenlos.",
+    "today": "Aujourd'hui",
+    "yesterday": "Hier",
+    "n-days": "il y a {0} jours",
+    "1-week": "il y a une semaine",
+    "n-weeks": "il y a {0} semaines",
+    "1-month": "il y a un mois",
+    "n-months": "il y a {0} mois",
+    "1-year": "il y a un an",
+    "n-years": "il y a {0} an"
+  },
 }
 </i18n>
