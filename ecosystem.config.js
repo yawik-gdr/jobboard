96f6d8c82c38f23675fe9f6e96b39c8fd0183459
Carsten Bleek <bleek@cross-solution.de>
2022-02-03 08:56:41 +0100

    checkout jobboard

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ apps : [{
     args: 'start',
     env: {
       NODE_ENV: "production",
-      PORT: 3000
+      PORT: 4000
     },
   }],
   
@@ -14,10 +14,10 @@ apps : [{
       user : 'yawik',
       host : 'cross-solution.de',
       ref  : 'origin/main',
-      repo : 'https://gitlab.com/cross-solution/homepage',
+      repo : 'https://gitlab.com/yawik/jobboard',
       path : '/home/yawik/yawik.org',
       'pre-deploy-local': 'echo "pre-deploy-local"',
-      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production -- --port 3000',
+      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production -- --port 4000',
       'pre-setup': 'pm2 ps'
     }
   }
